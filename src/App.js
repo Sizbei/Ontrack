a3ca341916b4416b09d28bb5fd3f8534b3be6167
Raymond Chen <Sizbei@users.noreply.github.com>
2021-06-25 23:27:42 -0400

    Added routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import MyFiles from './frontend/MyFiles.js'
+import { NavLink, Switch, Route, useParams } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-function App() {
-  return (
-    <MyFiles/>
-  );
-}
+const App = () => (
+  <div className="app">
+    <img src={logo} alt="Ontrack" class="logo" />
+    <Main />
+  </div>
+);
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={Home}></Route>
+    <Route exact path="/404" component={PageNotFound}></Route>
+    <Route exact path="/receivedlink/:fileId" component={Received}></Route>
+  </Switch>
+);
+
+const Home = () => (
+  <div className="home center">
+ 
+  </div>
+);
 
 export default App;
