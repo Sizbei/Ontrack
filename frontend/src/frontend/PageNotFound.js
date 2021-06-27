a1e88d61e89369bc2bcebf475f10ace8eee986db
Raymond Chen <Sizbei@users.noreply.github.com>
2021-06-26 22:36:05 -0400

    Finished 404 page

--- frontend/src/frontend/PageNotFound.js
+++ frontend/src/frontend/PageNotFound.js
@@ -1,9 +1,11 @@
-
+import error from '../res/images/Bigger icons/404.png'
+import './styles/PageNotFound.css'
 
 const PageNotFound = () => {
     return (
-      <div>
-          <p>yeet</p>
+      <div class = "pnf-error">
+          <div ><img src={error} alt="Error"/></div>
+          <div class = "pnf-text">This is not the web page you were looking for!😢 </div>
       </div>
     );
   };
